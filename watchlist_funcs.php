85cdb35d4e0e005627606e6670722f51d5b351f3
JordanJWSmith <73072693+JordanJWSmith@users.noreply.github.com>
2020-11-14 11:06:59 +0000

    added watchlist function
    Watchlist function page with queries for inserting/removing from Watchlist table

--- watchlist_funcs.php
+++ watchlist_funcs.php
@@ -1,19 +1,31 @@
  <?php
+ include_once("mysqli.php");
+ session_start();
+
 
 if (!isset($_POST['functionname']) || !isset($_POST['arguments'])) {
   return;
 }
 
 // Extract arguments from the POST variables:
-$item_id = $_POST['arguments'];
+//$item_id = $_POST['arguments']; // This just returns the datatype ('Array'), not sure how to extract the variable itself
+$item_id = $_SESSION['itemID'];  // This works though
+$userID = $_SESSION['userID'];
 
 if ($_POST['functionname'] == "add_to_watchlist") {
-  // TODO: Update database and return success/failure.
+  //TODO: Update database and return success/failure.
+    $addwatchlistquery = "INSERT INTO Watchlist (userID, itemID)
+             VALUES ('".$userID."', '".$item_id."')";
+
+    $connection->query($addwatchlistquery);
 
   $res = "success";
 }
 else if ($_POST['functionname'] == "remove_from_watchlist") {
   // TODO: Update database and return success/failure.
+    $removewatchlistquery = "DELETE FROM Watchlist WHERE userID = '".$userID."'
+                            AND itemID = '".$item_id."' ";
+    $connection->query($removewatchlistquery);
 
   $res = "success";
 }
