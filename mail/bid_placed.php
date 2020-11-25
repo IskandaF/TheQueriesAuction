2747e99dd05a134e95f9d29671416bc4d42debc4
“IskandaF” <ifakhritdinov@gmail.com>
2020-11-25 19:46:34 +0000

    deleted API key from Sendgrid

--- mail/bid_placed.php
+++ mail/bid_placed.php
@@ -3,6 +3,7 @@
 // require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
 require("sendgrid-php/sendgrid-php.php");
+require("/Users/soulist/Sites/UCL Computer Science/sendgridkey.php");
 // If not using Composer, uncomment the above line and
 // download sendgrid-php.zip from the latest release here,
 // replacing <PATH TO> with the path to the sendgrid-php.php file,
@@ -13,7 +14,7 @@ $email = new \SendGrid\Mail\Mail();
 $email->setFrom("auctionthequeries@gmail.com", "The Queries Auction");
 $email->setSubject("You placed the bid");
 
-$sendgrid = new \SendGrid("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");
+
 
 function sendBidPlacedEmail($email,$sendgrid){
 try {
