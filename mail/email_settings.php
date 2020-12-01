51d82974af94e2fdf9b62e3a01b02869365951ab
JordanJWSmith <73072693+JordanJWSmith@users.noreply.github.com>
2020-12-01 12:08:01 +0000

    removed verbose output

--- mail/email_settings.php
+++ mail/email_settings.php
@@ -17,9 +17,9 @@ function createEmail(){
 
 function sendEmail($email){
 try {
-$email->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+$email->SMTPDebug = 0;                      // Enable verbose debug output
 $email->isSMTP();                                            // Send using SMTP
-$email->SMTPDebug  = 1;  
+$email->SMTPDebug  = 0;
 $email->SMTPAuth   = TRUE;
 $email->Port       = 587;
 $email->Host       = "smtp.gmail.com";
@@ -28,17 +28,15 @@ $email->Password   = "zazwav-3zeqhu-wicdeQ";                          // Enable 
                              // SMTP password
 $email->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 $email->setFrom("auctionthequeries@gmail.com", "The Queries Auction");
-$email->isHTML(true);  
+$email->isHTML(true);
 
 $email->CharSet = 'UTF-8';
 
 
 $email->send();
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$email->ErrorInfo}";
+    //echo "Message could not be sent. Mailer Error: {$email->ErrorInfo}";
 }
 }
 
 ;
-
-
